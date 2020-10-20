08e674bfef1f5e1a9963c8746ec108bf3dccc8ec
student <student@C429U20>
2020-10-20 17:23:04 +0300

    111

---
+++ TOMOGOCHI/Back/tomogochi-brain/Controllers/MessageController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace tomogochi_brain.Controllers
+{
+
+    [EnableCors("CorsApi")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class MessageController : ControllerBase
+    {
+        static MessagesClass ms = new MessagesClass();
+        //// GET: api/<Message>
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    string json = JsonSerializer.Serialize(ms.gmessages.ElementAt(0)); 
+        //    return  json.ToString() ;
+        //}
+
+        // GET api/<Message>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string json = "not found";
+            if ((id < ms.messages.Count) && (id>=0))
+            {
+                 json = JsonSerializer.Serialize(ms.messages.ElementAt(id));
+            }
+            return json.ToString();
+        }
+
+        // POST api/<Message>
+        [HttpPost]
+        public void Post([FromBody] message msg)
+        {
+            ms.Add(msg.username,msg.text);
+        }
+
+        //// PUT api/<Message>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<MessageController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ TOMOGOCHI/Back/tomogochi-brain/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace tomogochi_brain
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsApi",
+                    builder => builder.WithOrigins("http://localhost:8080", "http://rusakovam.su:8080")
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+            });
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseCors("CorsApi");
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                
+            });
+        }
+    }
+}
