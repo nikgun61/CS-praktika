3e19f7546dbdfd45c5d30a0be57941b5853557e0
student <student@C429U20>
2020-10-20 17:20:40 +0300

    my first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for(int i=0; i < 100; i++)
+      {
+        Console.Beep(2200-i*20, 300-2*i);
+        Console.WriteLine("ya ne konchenniy!");
+        
+      }
+      Console.Beep(3000, 40000);
+    }
+  }
+}
